59e7e2b53bb22b08162d546fb2543378f5098dba
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2023-11-12 11:30:07 +0300

    methods of Array

--- Basic_JS/array.js
+++ Basic_JS/array.js
@@ -18,5 +18,31 @@ console.log(number_array[0]);
 
 // for of loop for in 
 
-for(const key in number_array) { console.log(key)}
-for(const key of number_array){console.log(key)}
+for(const key in number_array) { console.log(key)}// index value of array 0,1,2,3,4,5 0,..n-1
+for(const key of number_array){console.log(key)} // array value 
+
+// bulging methods 
+/**
+ * push(data) value add at the last of array
+ * unshift(data) value add at the beginig of array
+ * 
+ * pop() last value remove
+ * shift() first value remove
+ * 
+ * map(function )
+ */
+
+console.log(number_array.push('Abel'));
+console.log(number_array);
+console.log(number_array.unshift('yassin'));
+console.log(number_array);
+
+console.log(number_array.pop());
+console.log(number_array);
+
+console.log(number_array.shift());
+console.log(number_array);
+
+const newArray= number_array.map(vlaue=>vlaue*2);
+console.log(newArray)
+
